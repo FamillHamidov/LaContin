26c23eda705d453fdb9c2aa961d1334a7ead347b
FamillHamidov <famil.hemid@gmail.com>
2023-09-15 19:55:14 +0400

    UpdateFront

---
+++ PresentationLayer/VC/DiscountedProduct/DiscountedProductViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.VC.DiscountedProduct
+{
+    public class DiscountedProductViewComponent:ViewComponent
+    {
+        private readonly IProductService _productService;
+
+        public DiscountedProductViewComponent(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public Task<IViewComponentResult> InvokeAsync()
+        {
+            var product = _productService.GetAll();
+            return Task.FromResult<IViewComponentResult>(View(product));
+        }
+    }
+}
