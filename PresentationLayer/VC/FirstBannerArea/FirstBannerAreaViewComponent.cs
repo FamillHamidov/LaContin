9ae16543542ebe8e9acb9a2affa9f37cc19467a8
FamillHamidov <famil.hemid@gmail.com>
2023-09-18 20:12:05 +0400

    ChangeHomePage

---
+++ PresentationLayer/VC/FirstBannerArea/FirstBannerAreaViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.VC.FirstBannerArea
+{
+	public class FirstBannerAreaViewComponent:ViewComponent
+	{
+		public Task<IViewComponentResult> InvokeAsync()
+		{
+			return Task.FromResult<IViewComponentResult>(View());
+		}
+	}
+}
---
+++ PresentationLayer/VC/SecondBannerArea/SecondBannerAreaViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.VC.SecondBannerArea
+{
+	public class SecondBannerAreaViewComponent:ViewComponent
+	{
+		public Task<IViewComponentResult> InvokeAsync()
+		{
+			return Task.FromResult<IViewComponentResult>(View());
+		}
+	}
+}
