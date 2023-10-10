ffbe59f4d2a8357357a1c8ac8bd774ebd8c55cbb
FamillHamidov <famil.hemid@gmail.com>
2023-10-10 21:11:22 +0400

    ShopPage

---
+++ PresentationLayer/Controllers/ShopController.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.Controllers
+{
+    public class ShopController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly Context _context;
+        public ShopController(Context context, IProductService productService)
+        {
+            _context = context;
+            _productService = productService;
+        }
+        public IActionResult Index(int id)
+        {
+            var products = _context.Products.Where(x => x.CategoryId == id).ToList();
+            return View(products);
+        }
+        public IActionResult ShopDetail(int id)
+        {
+            var product = _productService.GetById(id);
+            return View(product);
+        }
+    }
+}
